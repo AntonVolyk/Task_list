a3d6779caf31bfd7584bfd14c220bf55f8ae33d7
AntonVolik <volik.anton@gmail.com>
2017-06-30 11:39:23 +0300

    Routing was added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { HttpModule }   from '@angular/http';
+import {Routes, RouterModule} from '@angular/router';
 import { AppComponent, TableComponent, TaskComponent} from './index';
 import {HighPriorityDirective} from './directives/index';
 
+// routes settings
+const appRoutes: Routes =[
+    { path: 'table', component: TableComponent},
+    { path: 'task', component: TaskComponent}
+    { path: '**', component: TableComponent }
+];
+
 @NgModule({
   imports: [
-    BrowserModule, HttpModule
+    BrowserModule, 
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     AppComponent, 
